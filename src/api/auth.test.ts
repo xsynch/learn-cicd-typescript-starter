afbc8c6c4688406860510de9cbc0ecc65ad926f1
xsynch <jdstrickland@gmail.com>
2025-06-12 22:05:21 -0400

    added tests for auth function

---
+++ src/api/auth.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "./auth";
+import { IncomingHttpHeaders } from "http";
+
+
+
+const headersWithApiKey: IncomingHttpHeaders = {
+   "authorization" : "ApiKey asdfasdfasdf",
+}
+
+
+const headersWithoutApiKey: IncomingHttpHeaders = {
+   "authorization" : " asdfasdfasdf",
+}
+
+const headersWithoutAuthorization: IncomingHttpHeaders = {
+    "content-type": " asdfasdfasdf",
+}
+
+test("Has Authorization and ApiKey",() => {
+    expect(getAPIKey(headersWithApiKey)).toBe("asdfasdfasdf")
+})
+
+test(" Authorization and No ApiKey",() => {
+    expect(getAPIKey(headersWithoutApiKey)).toBeNull()
+})
+
+test(" No Authorization and No ApiKey",() => {
+    expect(getAPIKey(headersWithoutAuthorization)).toBeNull()
+})
